6bac5d8ff7442d72e45a1a4b0165a5b08c22dee6
Shrajan Tiwari <shrajan28@gmail.com>
2024-05-21 16:32:16 +0530

    Updates

--- src/Components/Partition.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Partition() {
-  return (
-    <div className="w-[4px] bg-gradient-to-b from-[#1A68DD] to-[#9EACC6]"></div>
-  );
-}
-
-export default Partition;
